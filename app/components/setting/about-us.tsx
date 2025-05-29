690c37f835caafcda6b327313d93e95482b5d5c1
monster1935 <monster.1935.li@gmail.com>
2025-05-29 13:41:52 +0800

    chore: optimize app update

--- app/components/setting/about-us.tsx
+++ app/components/setting/about-us.tsx
@@ -4,27 +4,35 @@ import Logo from "../../icons/aiden-logo.svg";
 import config from "@/src-tauri/tauri.conf.json";
 import RightIcon from "../../icons/right-arrow.svg";
 import { useAppUpdate } from "@/app/hooks/use-app-update";
+import { showConfirm } from "@/app/components/confirm-modal/confirm";
 
 export default function AboutUs() {
-  const { isShowUpdate, handleUpdate, isUpdating } = useAppUpdate();
+  const { isShowUpdate, isLatest, handleUpdate, isUpdating } = useAppUpdate();
 
   const aboutUsArr = [
-    { show: true, name: "Export log", onClick: () => exportAndDownloadLog() },
     {
-      show: isShowUpdate,
       name: isUpdating ? "Updating..." : "Update to latest version",
       onClick: () => {
         if (isUpdating) return;
+        if (isLatest || !isShowUpdate) {
+          showConfirm({
+            title: "",
+            description: `The current v ${config.package.version} is already the latest version.`,
+            type: "latestVersion",
+            noClose: true,
+            withLogo: true,
+          });
+          return;
+        }
         handleUpdate();
       },
     },
+    { name: "Export log", onClick: () => exportAndDownloadLog() },
     {
-      show: true,
       name: "User Agreement",
       onClick: () => shell.open("https://aidenai.io/terms-of-service.html"),
     },
     {
-      show: true,
       name: "Privacy Policy",
       onClick: () => shell.open("https://aidenai.io/privacy.html"),
     },
@@ -42,7 +50,6 @@ export default function AboutUs() {
       </div>
       <div className="flex flex-col">
         {aboutUsArr.map((item) => {
-          if (!item.show) return null;
           return (
             <div
               key={item.name}
