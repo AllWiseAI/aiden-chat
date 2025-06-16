b6c5504a6ae4f6776e6f485c9ffe269185ea07db
monster1935 <monster.1935.li@gmail.com>
2025-06-16 20:52:38 +0800

    chore: update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -54,7 +54,7 @@ fn get_host_server_path<R: Runtime>(app: &AppHandle<R>) -> PathBuf {
     let binary_name = "host_server_linux";
 
     #[cfg(target_os = "windows")]
-    let binary_name = "host_server_windows.exe";
+    let binary_name = "host_server_windows";
 
     #[cfg(not(any(target_os = "macos", target_os = "linux", target_os = "windows")))]
     let binary_name = "host_server_macos";
