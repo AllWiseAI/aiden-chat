d362478b3fbf015741edd0744798981858e042c2
monster1935 <monster.1935.li@gmail.com>
2025-07-03 14:29:18 +0800

    feat: enable get domain by region

--- app/services/model.ts
+++ app/services/model.ts
@@ -1,12 +1,8 @@
 import { aidenFetch as fetch } from "@/app/utils/fetch";
 import { ModelOption } from "../typing";
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "https://dev.aidenai.io"
-    : "https://prod.aidenai.io";
 
 export const getModelList = async (): Promise<ModelOption[]> => {
-  const { data, status } = await fetch(`${baseURL}/api/v1/models`, {
+  const { data, status } = await fetch("/api/v1/models", {
     method: "GET",
     body: {
       type: "Json",
