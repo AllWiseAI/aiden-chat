d5380ba3e12561aa0542e2257adf5c4887c412ce
monster1935 <monster.1935.li@gmail.com>
2025-07-15 10:48:39 +0800

    fix: fix token error (#130)

--- app/hooks/use-server-ready.ts
+++ app/hooks/use-server-ready.ts
@@ -2,14 +2,25 @@ import { useEffect } from "react";
 import { listen } from "@tauri-apps/api/event";
 import { HOST_SERVER_READY_EVENT } from "../constant";
 import { useAppConfig } from "../store/config";
+import { getLocalToken } from "../services";
 
 const LOADING_TIMEOUT = Number(
   process.env.NEXT_PUBLIC_LOADING_TIMEOUT || 40000,
 );
 
 let resolved = false;
+
 export function useHostServerReady(onReady: (ready: boolean) => void) {
   const config = useAppConfig.getState();
+  async function getToken() {
+    try {
+      const token = await getLocalToken();
+      const { data } = token;
+      config.setLocalToken(data);
+    } catch (error) {
+      console.log("getLocalToken error", error);
+    }
+  }
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (!resolved) {
@@ -22,6 +33,7 @@ export function useHostServerReady(onReady: (ready: boolean) => void) {
     const unlistenPromise = listen(HOST_SERVER_READY_EVENT, (event) => {
       const port = event.payload as number | undefined;
       console.log("Host server ready, port ", port);
+
       if (!port) {
         console.log("Host server ready, but port is undefined");
         clearTimeout(timeout);
@@ -29,6 +41,7 @@ export function useHostServerReady(onReady: (ready: boolean) => void) {
         return;
       }
       config.setHostPort(port);
+      getToken();
       if (!resolved) {
         resolved = true;
         clearTimeout(timeout);
