c3fb0cb0cbdec3f765c9257eb97c43d1f30c8d14
monster1935 <monster.1935.li@gmail.com>
2025-07-31 09:40:24 +0800

    chore: update task

--- app/hooks/use-websocket.ts
+++ app/hooks/use-websocket.ts
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { websocketManager } from "../utils/websocket";
 import { useAppConfig } from "../store/config";
 import { showNotification } from "../utils/notification";
+import { toast } from "sonner";
 
 export const useWebSocket = () => {
   const port = useAppConfig.getState().hostServerPort;
@@ -12,6 +13,14 @@ export const useWebSocket = () => {
     websocketManager.onMessage((msg) => {
       console.log("WebSocket message:", msg);
 
+      if (msg.type === "task_failed") {
+        // @ts-ignore
+        toast.error(`任务"${msg.task_name}"失败`);
+        // @ts-ignore
+      } else if (msg.type === "task_success") {
+        // @ts-ignore
+        toast.success(`任务"${msg.task_name}"成功`);
+      }
       showNotification({
         title: msg.type,
         body: msg.task_description,
