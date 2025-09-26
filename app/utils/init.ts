3c5159772dde7745a13560c95b6052ebaad02a89
monster1935 <monster.1935.li@gmail.com>
2025-09-26 10:31:41 +0800

    feat: use worker to init websocket and resolve ws messages

--- app/utils/init.ts
+++ app/utils/init.ts
@@ -2,63 +2,57 @@ import { initLocalToken } from "../utils/fetch";
 import { useMcpStore } from "../store/mcp";
 import { useSettingStore } from "../store/setting";
 import { useAppConfig } from "../store/config";
-import { showNotification } from "../utils/notification";
-import {
-  TaskFailed,
-  websocketManager,
-  TaskCompletedOrTested,
-} from "../utils/websocket";
-import { getHeaders } from "../utils/fetch";
-import { track, EventName } from "../utils/analysis";
 
-const titleMap = {
-  task_completed: "Task Completed",
-  task_failed: "Task Failed",
-  task_tested: "Task Tested",
-  get_latest_refresh_token: "Get Latest Refresh Token",
-  analytics_event: "Analytics Event",
-};
+import { track } from "../utils/analysis";
+
 let websocketInitialized = false;
 
-const initWebsocket = () => {
+const initWebsocketWorker = () => {
+  console.log("[Main][Websocket] init websocket worker");
   if (websocketInitialized) {
-    console.warn("WebSocket already initialized, skipping.");
+    console.warn("[Main][Websocket] WebSocket already initialized, skipping.");
     return;
   }
-  websocketInitialized = true;
-  websocketManager.clearListeners();
+  const wsWorker = new Worker(new URL("../ws.worker.ts", import.meta.url), {
+    type: "module",
+  });
+
   const port = useAppConfig.getState().hostServerPort;
   const localToken = useAppConfig.getState().localToken;
-  websocketManager.connect(port, localToken);
-  websocketManager.onMessage(async (msg) => {
-    if (msg.type === "get_latest_refresh_token") {
-      console.log("[websocket] get_latest_refresh_token", msg);
-      const headers = await getHeaders({ aiden: true });
-      websocketManager.send({
-        type: "update_refresh_token",
-        token: headers["Aiden-Authorization"],
-      });
-    }
-    if (msg.type === "analytics_event") {
-      console.log("[websocket] analytics_event", msg);
-      const { event_name, params } = msg;
-      track(event_name as EventName, params);
-    }
-    if (["task_completed", "task_failed", "task_tested"].includes(msg.type)) {
-      console.log("[websocket] task result: ", msg.type);
-      showNotification({
-        title: titleMap[msg.type] ?? msg.type,
-        body: (msg as TaskCompletedOrTested | TaskFailed).task_description,
-      });
-    }
+
+  wsWorker.postMessage({
+    type: "connect",
+    payload: { port: port, localToken: localToken },
   });
+
+  wsWorker.postMessage({
+    type: "send",
+    payload: { type: "ping" },
+  });
+
+  wsWorker.onmessage = (e) => {
+    const { type, payload, message } = e.data;
+
+    console.log("[Main][Websocket] onmessage type:", type);
+
+    if (type === "status") {
+      console.log("[Main][Websocket] status:", message);
+      websocketInitialized = true;
+    }
+
+    if (type === "analytics_event") {
+      console.log("[Main][Websocket] analytics_event:", payload);
+      const { event_name, params } = payload;
+      track(event_name, params);
+    }
+  };
 };
 
 export const appDataInit = async () => {
   await initLocalToken();
   useMcpStore.getState().init();
   useAppConfig.getState().initModelList();
-  initWebsocket();
+  initWebsocketWorker();
   const getRegion = useSettingStore.getState().getRegion;
   getRegion();
 };
