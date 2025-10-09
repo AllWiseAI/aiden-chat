f2608be4c03b4afe36d82474495db177f3ae8fb2
monster1935 <monster.1935.li@gmail.com>
2025-10-09 15:22:20 +0800

    chore: update subscription text

--- app/components/setting/subscription.tsx
+++ app/components/setting/subscription.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState, useMemo } from "react";
 import { Dialog, DialogOverlay } from "../shadcn/dialog";
 import { useTranslation } from "react-i18next";
 import { apiGetUserPlan } from "@/app/services";
-import SuccessIcon from "../../icons/success.svg";
-import FailedIcon from "../../icons/close.svg";
 
 type Plan = "free" | "standard" | "pro";
 
@@ -50,7 +48,7 @@ export default function Subscription() {
             onClick={() => setShowPlan(false)}
           >
             <div
-              className="relative flex flex-col gap-6 p-6 w-72 h-127 text-black bg-[#ECEFF3] rounded-xl overflow-hidden"
+              className="relative flex flex-col gap-4 p-6 w-77 h-146 overflow-y-auto overflow-hidden text-black bg-[#ECEFF3] rounded-xl"
               onClick={(e) => e.stopPropagation()}
             >
               <div
@@ -60,7 +58,7 @@ export default function Subscription() {
                     "radial-gradient(circle at top right, rgba(0, 212, 126, 1) 0%, rgba(0, 212, 126, 0) 70%)",
                 }}
               ></div>
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
                 <p className="text-[32px] font-light z-1">
                   {t("subscription.free.title") + t("subscription.planT")}
                 </p>
@@ -82,29 +80,57 @@ export default function Subscription() {
 
               <div className="flex justify-between gap-4 items-center">
                 <div className="flex-1 h-[1px] bg-[#C1C7D0] -ml-6"></div>
-                <span className="text-lg text-[#6F6F6F]">
+                <span className="text-lg text-[#6F6F6F] font-normal">
                   {t("subscription.included")}
                 </span>
                 <div className="flex-1 h-[1px] bg-[#C1C7D0] -mr-6"></div>
               </div>
-              <div className="flex flex-col gap-4 text-sm">
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0" />
-                  <span>{t("subscription.free.included.1")}</span>
+              <div className="flex flex-col gap-3 text-sm">
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature1")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.free.feature1Text")}
+                  </span>
                 </div>
 
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0" />
-                  <span>{t("subscription.free.included.2")}</span>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature2")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.free.feature2Text")}
+                  </span>
                 </div>
-                <div className="flex items-center gap-3">
-                  <FailedIcon className="size-6 shrink-0 text-[#4ADE80]" />
-                  <span>{t("subscription.free.included.3")}</span>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature3")}
+                  </span>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-normal">
+                      {t("subscription.free.feature3Text.1")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.free.feature3Text.2")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.free.feature3Text.3")}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature4")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.free.feature4Text")}
+                  </span>
                 </div>
               </div>
             </div>
             <div
-              className="relative flex flex-col gap-6 p-6 w-72 h-127 text-white bg-[#101213] rounded-xl overflow-hidden"
+              className="relative flex flex-col gap-4 p-6 w-77 h-146 overflow-y-auto overflow-hidden text-white bg-[#101213] rounded-xl"
               onClick={(e) => e.stopPropagation()}
             >
               <div
@@ -114,7 +140,7 @@ export default function Subscription() {
                     "radial-gradient(circle at top right, rgba(0, 212, 126, 1) 0%, rgba(0, 212, 126, 0) 70%)",
                 }}
               ></div>
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
                 <p className="text-[32px] font-light z-1">
                   <span className="text-[#C8FF85]">
                     {t("subscription.standard.title")}
@@ -139,29 +165,60 @@ export default function Subscription() {
 
               <div className="flex justify-between gap-4 items-center">
                 <div className="flex-1 h-[1px] bg-[#C1C7D0] -ml-6"></div>
-                <span className="text-lg text-[#6F6F6F]">
+                <span className="text-lg text-[#6F6F6F] font-normal">
                   {t("subscription.included")}
                 </span>
                 <div className="flex-1 h-[1px] bg-[#C1C7D0] -mr-6"></div>
               </div>
-              <div className="flex flex-col gap-4 text-sm text-[#00D47E]">
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0" />
-                  <span>{t("subscription.standard.included.1")}</span>
+              <div className="flex flex-col gap-3 text-sm text-[#FEFEFE]/80">
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature1")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.standard.feature1Text")}
+                  </span>
                 </div>
 
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0" />
-                  <span>{t("subscription.standard.included.2")}</span>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature2")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.standard.feature2Text")}
+                  </span>
+                  <span className="text-[#C7FF85] font-semibold">
+                    {t("subscription.standard.feature2Amount")}
+                  </span>
                 </div>
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0 " />
-                  <span>{t("subscription.standard.included.3")}</span>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature3")}
+                  </span>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-normal">
+                      {t("subscription.standard.feature3Text.1")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.standard.feature3Text.2")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.standard.feature3Text.3")}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature4")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.standard.feature4Text")}
+                  </span>
                 </div>
               </div>
             </div>
             <div
-              className="relative flex flex-col gap-6 p-6 w-72 h-127 text-white bg-[#101213] rounded-xl overflow-hidden"
+              className="relative flex flex-col gap-4 p-6 w-77 h-146 text-white bg-[#101213] rounded-xl overflow-hidden"
               onClick={(e) => e.stopPropagation()}
             >
               <div
@@ -178,7 +235,7 @@ export default function Subscription() {
                     "radial-gradient(circle at left bottom, rgba(99, 0, 212, 1) 0%, rgba(99, 0, 212, 0) 70%)",
                 }}
               ></div>
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
                 <p className="text-[32px] font-light">
                   <span className="text-[#C89CFF]">
                     {t("subscription.pro.title")}
@@ -203,24 +260,56 @@ export default function Subscription() {
 
               <div className="flex justify-between gap-4 items-center">
                 <div className="flex-1 h-[1px] bg-[#C1C7D0] -ml-6"></div>
-                <span className="text-lg text-[#6F6F6F]">
+                <span className="text-lg text-[#6F6F6F] font-normal">
                   {t("subscription.included")}
                 </span>
                 <div className="flex-1 h-[1px] bg-[#C1C7D0] -mr-6"></div>
               </div>
-              <div className="flex flex-col gap-4 text-sm text-[#C8B1FF]">
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0 text-[#4ADE80]" />
-                  <span> {t("subscription.pro.included.1")}</span>
+              <div className="flex flex-col gap-2 text-sm text-[#FEFEFE]/80">
+                <div>
+                  <span className="text-[#744ADE] mr-1">
+                    {t("subscription.feature1")}
+                  </span>
+                  <span>{t("subscription.pro.feature1Text")}</span>
                 </div>
 
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0 text-[#4ADE80]" />
-                  <span>{t("subscription.pro.included.2")}</span>
+                <div>
+                  <span className="text-[#744ADE] font-semibold mr-1">
+                    {t("subscription.feature2")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.pro.feature2Text")}
+                  </span>
+                  <span className="font-semibold text-[#FF5DE1]">
+                    {t("subscription.pro.feature2Amount")}
+                  </span>
                 </div>
-                <div className="flex items-center gap-3">
-                  <SuccessIcon className="size-6 shrink-0 text-[#4ADE80]" />
-                  <span>{t("subscription.pro.included.3")}</span>
+                <div>
+                  <span className="text-[#744ADE] font-semibold mr-1">
+                    {t("subscription.feature3")}
+                  </span>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-normal">
+                      {t("subscription.pro.feature3Text.1")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.pro.feature3Text.2")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.pro.feature3Text.3")}
+                    </span>
+                    <span className="font-normal">
+                      {t("subscription.pro.feature3Text.4")}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <span className="text-primary font-semibold mr-1">
+                    {t("subscription.feature4")}
+                  </span>
+                  <span className="font-normal">
+                    {t("subscription.standard.feature4Text")}
+                  </span>
                 </div>
               </div>
             </div>
