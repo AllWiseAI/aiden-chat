c831ee148a3f4e21866c005af6ac33585fd96c59
monster1935 <monster.1935.li@gmail.com>
2025-10-11 11:04:23 +0800

    chore: update

--- app/components/new-task.tsx
+++ app/components/new-task.tsx
@@ -4,7 +4,6 @@ import TaskManagement from "./task-management";
 import { WindowHeader } from "./window-header";
 import { ModelSelect } from "../components/model-select";
 import { useAppConfig } from "../store";
-import { TestTaskInfo } from "../typing";
 
 export function NewTask() {
   const defaultModel = useAppConfig((s) => s.defaultModel);
@@ -12,21 +11,13 @@ export function NewTask() {
   const handleModelChange = (model: string) => {
     setModel(model);
   };
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [testTaskList, setTestTaskList] = useState<TestTaskInfo[]>([]);
-
-  const handleTestChange = (testTaskInfo: TestTaskInfo) => {
-    console.log("testTaskInfo===", testTaskInfo);
-    setTestTaskList([testTaskInfo]);
-  };
   return (
     <div>
       <WindowHeader>
         <ModelSelect mode="custom" onChange={handleModelChange} value={model} />
       </WindowHeader>
       <div className="flex flex-col gap-5 px-15 py-5 border-t border-[#E8ECEF] dark:border-[#232627]/50">
-        <TaskManagement model={model} onTestChange={handleTestChange} />
+        <TaskManagement model={model} />
       </div>
     </div>
   );
